676800ae7660df8f1ff7f3cffbb0e3fd8e659d42
jeremy miller <runofthemill@users.noreply.github.com>
2018-01-26 22:23:22 -0800

    install.sh updates

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Installing Xcode CLI Tools..."
+
+xcode-select --install
+sudo xcodebuild -license accept
+
 echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update Homebrew recipes
@@ -17,18 +25,10 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-# Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet
-
-# Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
-
 # Install global NPM packages
 npm install --global yarn
+
+. "$DOTFILES_DIR/npm.sh"
 
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
@@ -38,5 +38,19 @@ mkdir $HOME/Sites
 ln -s ./.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
+# -# We will run this last because this will reload the shell
+# -source .macos
+
+echo ""
+echo "Have you set up an ssh key for Git?  (y/n)"
+read -r response
+if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Enter email address for ssh key"
+    read email
+    ssh-keygen -t rsa -C "$email"
+    #copy ssh key to clipboard
+    pbcopy < ~/.ssh/id_rsa.pub
+    echo "SSH key copied to clipboard"
+fi
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
