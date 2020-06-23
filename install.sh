31bf3535078e67ff00d9d9bff084da8dca4f545e
Jeremy Miller <jeremy.miller@wirecutter.com>
2020-06-23 10:37:39 -0700

    update homebrew install

--- install.sh
+++ install.sh
@@ -103,8 +103,8 @@ echo "Setting up your Mac..."
 
 echo "Installing Homebrew..."
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo "Updating Homebrew..."
